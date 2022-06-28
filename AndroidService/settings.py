f09a2c69275683a85792be0b72af0a18b470bf12
Ernesto Suarez <ernesto@amicitech.com>
2022-06-27 23:43:24 -0500

    update

--- AndroidService/settings.py
+++ AndroidService/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$xinft*pi_u(2nr0_3cqxuea^9117=6wik0@8$59g@b3unt^ic'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
