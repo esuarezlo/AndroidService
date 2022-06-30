9d6ba238d1e8bd06406c35b07e90344d27e7612e
Ernesto Suarez <ernesto@amicitech.com>
2022-06-30 11:07:12 -0500

    update

--- danpservice/views.py
+++ danpservice/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import sqlite3
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 # Create your views here.
@@ -12,13 +13,11 @@ def index(request):
 @csrf_exempt
 def addtoken(request):
     if request.method == 'POST':
-        data = request.body
-        # userid = request.POST["userid"]
-        # token = request.POST["token"]
-        # dict = {
-        #     'userid': userid,
-        #     'token': token
-        # }
+        dataJson = json.loads(request.body.decode('utf-8'))
+        dict = {
+            'userid': dataJson['userid'],
+            'token': dataJson['token']
+        }
         # con = sqlite3.connect('example.db')
         # cur = con.cursor()
         #
@@ -27,12 +26,12 @@ def addtoken(request):
         # con.commit()
 
     elif request.method == 'GET':
-        data ={ "get method":" no token"}
+        dict ={ "get method":" no token"}
 
-    #return JsonResponse(dict, safe=False)
+    return JsonResponse(dict, safe=False)
 
     #print(str(data, 'UTF-8'))
-    return HttpResponse(str(data, 'UTF-8'), content_type="application/json")
+    #return HttpResponse(str(data, 'UTF-8'), content_type="application/json")
 
 
 def sendmessage(request):
