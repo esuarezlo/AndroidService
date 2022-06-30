27a8b5459e694305a2df8f57d4e5657f9cc856d4
Ernesto Suarez <ernesto@amicitech.com>
2022-06-30 08:00:27 -0500

    update

--- danpservice/views.py
+++ danpservice/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-
+import sqlite3
 
 # Create your views here.
 def index(request):
@@ -15,11 +15,17 @@ def addtoken(request):
             'userid': userid,
             'token': token
         }
+        con = sqlite3.connect('example.db')
+        cur = con.cursor()
+
+        # Create table
+        cur.execute('''CREATE TABLE tokens (token text)''')
+        con.commit()
 
     elif request.method == 'GET':
         token = "get method, no token"
 
-    return HttpResponse(token)
+    return HttpResponse("token")
 
 
 def sendmessage(request):
